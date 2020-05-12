cca92109f74e4efa1f9514016d000b817e647e09
rajmohan77 <rajmohan3101993@gmail.com>
2020-05-13 00:22:23 +0530

    Detail popup setup

--- pokemon-app-phable/src/components/pokemon.js
+++ pokemon-app-phable/src/components/pokemon.js
@@ -1,24 +1,63 @@
 import React, { PureComponent } from 'react'
+import Modal from 'react-awesome-modal'
 
 class Pokemon extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedPokemonData: [],
+      showPokemonDetailModal: false
+    }
+  }
+
+  getPokemonDetails = url => {
+    return fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+      })
+      .then(data => {
+        this.setState({
+          showPokemonDetailModal: true,
+          selectedPokemonData: data
+        })
+      })
+  }
   render() {
     const { pokemon } = this.props
     return (
-      <div className="pokemon">
-        <button
-          type="button"
-          className="pokemon__sprite"
-          style={{
-            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              pokemon.id
-            }.png`})`,
-            cursor: "pointer"
-          }}
-          onClick={() => {
-            console.log('pooooooooooooo', pokemon.url)
-          }}
-        />
-        <p className="pokemon__name">{pokemon.name}</p>
+      <div>
+        <div className="pokemon">
+          <button
+            type="button"
+            className="pokemon__sprite"
+            style={{
+              backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                pokemon.id
+              }.png`})`,
+              cursor: 'pointer'
+            }}
+            onClick={() => {
+              this.getPokemonDetails(pokemon.url)
+            }}
+          />
+          <p className="pokemon__name">{pokemon.name}</p>
+        </div>
+
+        <Modal
+          visible={this.state.showPokemonDetailModal}
+          width="1000px"
+          height="auto"
+          effect="fadeInDown"
+          onClickAway={() =>
+            this.setState({
+              showPokemonDetailModal: !this.state.showPokemonDetailModal
+            })
+          }
+        >
+          <div style={{ marginTop: '0px' }}>Hey</div>
+        </Modal>
       </div>
     )
   }
